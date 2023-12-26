3f206f9c9dbad89285d92936e8b4aa39f9f72f4a
chenlin014 <1chen0bryan@gmail.com>
2023-12-25 20:17:56 -0800

    新郑简码
    一字最多四根

---
+++ simp_map.py
@@ -0,0 +1,24 @@
+import re
+
+def simp_code(code):
+    suffix = re.search(r'[简正左下异和重]+', code)
+    if suffix:
+        suffix = suffix.group()
+        code = code.replace(suffix, '')
+    else:
+        suffix = ''
+
+    if len(code) <= 4:
+        return code + suffix
+
+    return code[:2] + code[-2:] + suffix
+
+
+with open('zhengma.txt', encoding='utf-8') as f:
+    mb = {text:simp_code(code) for text, code in (
+        line.split('\t') for line in f.read().splitlines()
+    )}
+
+with open('xingzheng.txt', 'w', encoding='utf-8') as f:
+    for text, code in mb.items():
+        f.write(f'{text}\t{code}\n')
