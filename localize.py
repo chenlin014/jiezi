01325700bf0053ea21c8ad364621436ea679d7b1
chenlin014 <1chen0bryan@gmail.com>
2023-12-27 12:07:13 -0800

    正简对修正
    localize.py: 14

--- localize.py
+++ localize.py
@@ -6,14 +6,19 @@ with open('xingzheng.txt', encoding='utf-8') as f:
 
     for zi, ma in reader:
         mb[zi] = ma
+code_ls = set(mb.values())
 
 # 正體
 with open('xingzheng_zt.txt', 'w', encoding='utf-8') as f:
     writer = csv.writer(f, delimiter='\t')
     for zi, ma in mb.items():
-        xma = ma.replace('简和', '简').replace('正', '')
-        xma = re.sub(r'[左下简和喃韩]', '重', xma)
-        writer.writerow([zi, xma])
+        if ma + '正' in code_ls:
+            ma += '重'
+
+        ma = ma.replace('简和', '简').replace('正', '')
+        ma = re.sub(r'[左下简和喃韩]', '重', ma)
+
+        writer.writerow([zi, ma])
 
 # 简体
 
