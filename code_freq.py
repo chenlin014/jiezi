c321c4bcde467fc8fa42155990894cd75bb8f25f
chenlin014 <1chen0bryan@gmail.com>
2024-01-29 20:29:02 -0800

    字根频率
    计算字根频率，用于裁减考量。

---
+++ code_freq.py
@@ -0,0 +1,14 @@
+import csv, sys
+
+with open(sys.argv[1], encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter='\t')
+
+    codes = ''.join([row[1] for row in reader])
+
+code_freq = {code:0 for code in set(codes)}
+for code in codes:
+    code_freq[code] += 1
+
+ranking = [(freq, code) for code, freq in code_freq.items()]
+ranking.sort()
+print(ranking)
