a193c55b021e16d488cbd57e4abc6e75553ac23f
chenlin <1chen0bryan@gmail.com>
2024-02-10 12:44:57 -0800

    当地化与字根表
    当地化修改。添加字根列表。

--- localize.py
+++ localize.py
@@ -3,7 +3,7 @@ import re
 with open('xingzheng.txt', encoding='utf-8') as f:
     lines = f.read().splitlines()
 mb = {line[0]:line[1] for line in (
-    line.split() for line in lines
+        line.split() for line in lines
     )}
 codes = set(mb.values())
 
@@ -17,7 +17,8 @@ with open('xingzheng_zt.txt', 'w', encoding='utf-8') as f:
         if zi == '鸊':
             xma = '尸口辛鳥能'
 
-        xma = xma.replace('简和', '重').replace('简', '重').replace('和', '重').replace('正', '')
+        xma = xma.replace('简和', '重').replace('简', '重').replace('正', '')
+        xma = re.sub(r'[和喃韩]', '重', xma)
         f.write(f'{zi}\t{xma}\n')
 
 # 简体
@@ -32,7 +33,8 @@ with open('xingzheng_jt.txt', 'w', encoding='utf-8') as f:
         if zi == '鸊':
             xma = '尸口辛鳥能'
 
-        xma = xma.replace('简和', '').replace('简', '').replace('和', '重').replace('正', '重')
+        xma = xma.replace('简和', '').replace('简', '').replace('正', '重')
+        xma = re.sub(r'[和喃韩]', '重', xma)
         f.write(f'{zi}\t{xma}\n')
 
 # 日本
@@ -50,4 +52,5 @@ with open('xingzheng_jp.txt', 'w', encoding='utf-8') as f:
             xma = '尸口辛鳥能'
 
         xma = xma.replace('简和', '').replace('和', '').replace('简', '重').replace('正', '')
+        xma = re.sub(r'[喃韩]', '重', xma)
         f.write(f'{zi}\t{xma}\n')
