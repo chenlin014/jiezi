725ea4a346a7ebf6c80ca2cda9771df88210b2c1
chenlin <1chen0bryan@gmail.com>
2024-03-22 22:54:38 -0700

    码表简化：取码法，直接输出。

--- simp_map.py
+++ simp_map.py
@@ -1,6 +1,6 @@
 import re
 
-def simp_code(code):
+def simp_code(code, method):
     suffix = re.search(r'[简正左下重能和喃韩]+', code)
     if suffix:
         suffix = suffix.group()
@@ -9,17 +9,27 @@ def simp_code(code):
         suffix = ''
     suffix = suffix.replace('左', '重').replace('下', '重')
     
-    if len(code) <= 4:
+    if len(code) <= len(method):
         return code + suffix
 
-    return code[:2] + code[-2:] + suffix
+    return ''.join(code[ind] for ind in method) + suffix
 
+def main():
+    import sys
+    if len(sys.argv) < 3:
+        print(f'{sys.argv[0]} <码表> <取码法>')
+        quit()
 
-with open('zhengma.txt', encoding='utf-8') as f:
-    mb = {text:simp_code(code) for text, code in (
-        line.split('\t') for line in f.read().splitlines()
-    )}
+    _, mb_path, method = sys.argv
+    method = tuple(int(ind) for ind in method.split(','))
 
-with open('xingzheng.txt', 'w', encoding='utf-8') as f:
-    for text, code in mb.items():
-        f.write(f'{text}\t{code}\n')
+    with open(mb_path, encoding='utf-8') as f:
+        simp_map = ((text, simp_code(code, method)) for text, code in (
+            line.split('\t') for line in f.read().splitlines()
+        ))
+
+    for text, code in simp_map:
+        print(f'{text}\t{code}')
+
+if __name__ == '__main__':
+    main()
