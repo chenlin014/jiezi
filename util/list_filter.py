0856983046b52386319d5ebdff22b4ef195d27a0
chenlin <1chen0bryan@gmail.com>
2024-04-03 08:17:25 -0700

    开始制作简体简码
    通用规范汉字表一级字

---
+++ util/list_filter.py
@@ -0,0 +1,13 @@
+import csv, re
+
+with open('xingzheng.tsv', encoding='utf_8') as f:
+    r = csv.reader(f, delimiter='\t')
+    mb = {zi:re.sub(r'[左下正简和喃韓]', '', ma) for zi, ma in r}
+
+with open('/home/chenlin/data/lang/zh/char_table/level-1.txt', encoding='utf_8') as f:
+    chars = set(f.read().splitlines())
+
+with open('xz.tsv', 'w', encoding='utf_8') as f:
+    g = ((zi, ma) for zi, ma in mb.items() if zi in chars and len(ma) > 2)
+    for zi, ma in g:
+        f.write(f'{zi}\t{ma}\n')
