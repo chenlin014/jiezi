cf79d1b463e11b690ccf5e6f2ba7998c3b932543
chenlin <1chen0bryan@gmail.com>
2024-04-11 23:07:49 -0700

    plover: 指令不加{&}

--- dict-gen/format.sh
+++ dict-gen/format.sh
@@ -2,14 +2,18 @@
 
 case $1 in
 	rime)
-		cat | sed 's/,//g' | sed 's/ //g'
+		cat | sed 's/<>//g; s/ | //g'
 		;;
 	algebra)
-		cat | sed -E 's/[ ,]//g' | sed -E 's/(.+)\t(.+)/- xform|^\2$|\1|/'
+		cat | sed 's/<>//g; s/ | //g' | sed -E 's/(.+)\t(.+)/- xform|^\2$|\1|/'
 		;;
 	plover)
 		echo "{"
-		cat | sed 's/ /\//g; s/,//g' | sed -E 's/(.+)\t(.+)/"\2": "{\&\1}",/' | sed '$ s/",/"/'
+		cat | sed 's/ | /\//g; s/<>//g' |
+			sed -E 's/(.+)\t(.+)/"\2": "\1",/' |
+			sed -E '$ s/,$//' |
+			perl -pe 's/: "(?!(\{.+\}|=))/: "{&/g;' |
+			sed -E 's/: "\{&(.+)"/: "\{\&\1}"/'
 		echo "}"
 		;;
 esac
