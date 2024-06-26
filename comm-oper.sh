a01167cf3df75e7cff5b78986fe93920e59e73d8
chenlin <1chen0bryan@gmail.com>
2024-06-26 07:09:34 -0700

    改错：呛

--- comm-oper.sh
+++ comm-oper.sh
@@ -2,7 +2,7 @@
 
 case $1 in
 	newvc)
-		python mb-tool/subset.py table/jianrong.tsv table/common-patch-$2.tsv --difference |
+		python mb-tool/subset.py table/jianrong.tsv table/common-std-$2.tsv --difference |
 			python mb-tool/subset.py char_set/common-$2
 		;;
 	nojm)
