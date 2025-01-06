f495774952444ae395e247edd2036ae4db471f15
chenlin <1chen0bryan@gmail.com>
2025-01-05 18:17:04 -0800

    严：亞丿 -> 一业丿

--- serial/gen_dict.sh
+++ serial/gen_dict.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python mb-tool/apply_mapping.py monokey/codemap/$1.json monokey/keymap/$2.json $3 | awk -f monokey/add_stem.awk
+python mb-tool/apply_mapping.py serial/codemap/$1.json serial/keymap/$2.json $3 | awk -f serial/add_stem.awk
